554e1fe5d9daed9ca3d4fc9ef3312cc1ca1f1eb1
AxLMedx <65353621+AxLMedx@users.noreply.github.com>
2020-08-17 12:27:53 +0200

    j

--- note.php
+++ note.php
@@ -13,106 +13,123 @@
         getAll(path);
     });
 </script>
+<script>
+    function folderbarToggle() {
+        let bleu = document.getElementById("folderToggleId");
+        bleu.classList.toggle("folderToggle");
+}
+</script>
 <div class="grid-container">
-    <div class="folderBar">
+    <div id="folderToggleId" class="folderBar">
         <div class="navHeader"></div>
         <div id="foldersandfiles"></div>
     </div>
     <div id="noteContainer" class="note">
         <div id="noteTextFormat">
-            <div class="formatBtnContainer">
-                <button class="formatBtn" id=""><i class="fas fa-share" style="transform: scaleX(-1)"></i></button>
-                <div class="formatBtnDisc">Undo</div>
-            </div>
-            <div class="formatBtnContainer">
-                <button class="formatBtn" id=""><i class="fas fa-share"></i></button>
-                <div class="formatBtnDisc">Redo</div>
-            </div>
-            <div class="formatBtnContainer">
-                <button class="formatBtn" id=""><i class="far fa-copy"></i></button>
-                <div class="formatBtnDisc">Copy</div>
-            </div>
-            <div class="formatBtnContainer">
-                <button class="formatBtn" id=""><i class="fas fa-cut"></i></button>
-                <div class="formatBtnDisc">Cut</div>
-            </div>
-            <div class="formatBtnContainer">
-                <button class="formatBtn" id=""><i class="far fa-clipboard"></i></button>
-                <div class="formatBtnDisc">Paste</div>
-            </div>
-            <div class="formatBtnDiv"></div>
-            <div class="formatBtnContainer">
-            <div class="fontTypeDropdown">
-                <button onclick="toggleDropdown()" class="dropbtn formatBtn"><i class="fas fa-font"></i></button>
-                <div id="fontTypeDropdown" class="dropdown-content">
-                    <input type="text" placeholder="Search.." id="fontTypeInput" onkeyup="filterFontType()">
-                    <button id="fontTypeArial" style="font-family: Arial;">Arial</button>
-                    <button id="fontTypeComicSans" style="font-family: Comic Sans;">Comic Sans</button>
+        <button onclick="folderbarToggle()"><i class="fas fa-angle-double-right"></i></button>
+            <div class="formatBtnGroup">
+                <div class="formatBtnContainer">
+                    <button class="formatBtn" id=""><i class="fas fa-share" style="transform: scaleX(-1)"></i></button>
+                    <div class="formatBtnDisc">Undo</div>
                 </div>
+                <div class="formatBtnContainer">
+                    <button class="formatBtn" id=""><i class="fas fa-share"></i></button>
+                    <div class="formatBtnDisc">Redo</div>
+                </div>
+                <div class="formatBtnContainer">
+                    <button class="formatBtn" id=""><i class="far fa-copy"></i></button>
+                    <div class="formatBtnDisc">Copy</div>
+                </div>
+                <div class="formatBtnContainer">
+                    <button class="formatBtn" id=""><i class="fas fa-cut"></i></button>
+                    <div class="formatBtnDisc">Cut</div>
+                </div>
+                <div class="formatBtnContainer">
+                    <button class="formatBtn" id=""><i class="far fa-clipboard"></i></button>
+                    <div class="formatBtnDisc">Paste</div>
+                </div>
+                <div class="formatBtnDiv"></div>
             </div>
+            <div class="formatBtnGroup">
+                <div class="formatBtnContainer">
+                <div class="fontTypeDropdown">
+                    <button onclick="toggleDropdown()" class="dropbtn formatBtn"><i class="fas fa-font"></i></button>
+                    <div id="fontTypeDropdown" class="dropdown-content">
+                        <input type="text" placeholder="Search.." id="fontTypeInput" onkeyup="filterFontType()">
+                        <button id="fontTypeArial" style="font-family: Arial;">Arial</button>
+                        <button id="fontTypeComicSans" style="font-family: Comic Sans;">Comic Sans</button>
+                    </div>
+                </div>
+                </div>
+                <div class="formatBtnContainer">
+                    <button class="formatBtn"><i class="fas fa-text-height"></i></button>
+                    <div class="formatBtnDisc">Font size</div>
+                </div>
+                <div class="formatBtnContainer">
+                    <button class="formatBtn" id=""><i class="fas fa-chevron-up"></i></button>
+                    <div class="formatBtnDisc">Increase font size</div>
+                </div>
+                <div class="formatBtnContainer">
+                    <button class="formatBtn" id=""><i class="fas fa-chevron-down"></i></button>
+                    <div class="formatBtnDisc">Decrease font size</div>
+                </div>
+                <div class="formatBtnDiv"></div>
             </div>
-            <div class="formatBtnContainer">
-                <button class="formatBtn"><i class="fas fa-text-height"></i></button>
-                <div class="formatBtnDisc">Font size</div>
+            <div class="formatBtnGroup">
+                <div class="formatBtnContainer">
+                    <button class="formatBtn" id="">fC</button>
+                    <div class="formatBtnDisc">Font color</div>
+                </div>
+                <div class="formatBtnContainer">
+                    <button class="formatBtn" id="">bC</button>
+                    <div class="formatBtnDisc">Back color</div>
+                </div>
+                <div class="formatBtnContainer">
+                    <button class="formatBtn" id="tagB"><i class="fas fa-bold"></i></button>
+                    <div style="font-weight: bold;" class="formatBtnDisc">Bold</div>
+                </div>
+                <div class="formatBtnContainer">
+                    <button class="formatBtn" id="tagI"><i class="fas fa-italic"></i></button>
+                    <div style="font-style: italic;" class="formatBtnDisc">Italic</div>
+                </div>
+                <div class="formatBtnContainer">
+                    <button class="formatBtn" id="tagU"><i class="fas fa-underline"></i></button>
+                    <div style="text-decoration:underline;" class="formatBtnDisc">Underline</div>
+                </div>
+                <div class="formatBtnContainer">
+                    <button class="formatBtn" id="tagS"><i class="fas fa-strikethrough"></i></button>
+                    <div style="text-decoration:line-through;" class="formatBtnDisc">Line through</div>
+                </div>
+                <div class="formatBtnDiv"></div>
             </div>
-            <div class="formatBtnContainer">
-                <button class="formatBtn" id=""><i class="fas fa-chevron-up"></i></button>
-                <div class="formatBtnDisc">Increase font size</div>
+            <div class="formatBtnGroup">
+                <div class="formatBtnContainer">
+                    <button class="formatBtn" id="tagJL"><i class="fas fa-align-left"></i></button>
+                    <div class="formatBtnDisc">Align text left</div>
+                </div>
+                <div class="formatBtnContainer">
+                    <button class="formatBtn" id="tagJC"><i class="fas fa-align-center"></i></button>
+                    <div class="formatBtnDisc">Align text center</div>
+                </div>
+                <div class="formatBtnContainer">
+                    <button class="formatBtn" id="tagJR"><i class="fas fa-align-right"></i></button>
+                    <div class="formatBtnDisc">Align text right</div>
+                </div>
+                <div class="formatBtnContainer">
+                    <button class="formatBtn" id="tagJF"><i class="fas fa-align-justify"></i></button>
+                    <div class="formatBtnDisc">Align text full</div>
+                </div>
+                <div class="formatBtnDiv"></div>
             </div>
-            <div class="formatBtnContainer">
-                <button class="formatBtn" id=""><i class="fas fa-chevron-down"></i></button>
-                <div class="formatBtnDisc">Decrease font size</div>
-            </div>
-            <div class="formatBtnDiv"></div>
-            <div class="formatBtnContainer">
-                <button class="formatBtn" id="">fC</button>
-                <div class="formatBtnDisc">Font color</div>
-            </div>
-            <div class="formatBtnContainer">
-                <button class="formatBtn" id="">bC</button>
-                <div class="formatBtnDisc">Back color</div>
-            </div>
-            <div class="formatBtnContainer">
-                <button class="formatBtn" id="tagB"><i class="fas fa-bold"></i></button>
-                <div style="font-weight: bold;" class="formatBtnDisc">Bold</div>
-            </div>
-            <div class="formatBtnContainer">
-                <button class="formatBtn" id="tagI"><i class="fas fa-italic"></i></button>
-                <div style="font-style: italic;" class="formatBtnDisc">Italic</div>
-            </div>
-            <div class="formatBtnContainer">
-                <button class="formatBtn" id="tagU"><i class="fas fa-underline"></i></button>
-                <div style="text-decoration:underline;" class="formatBtnDisc">Underline</div>
-            </div>
-            <div class="formatBtnContainer">
-                <button class="formatBtn" id="tagS"><i class="fas fa-strikethrough"></i></button>
-                <div style="text-decoration:line-through;" class="formatBtnDisc">Line through</div>
-            </div>
-            <div class="formatBtnDiv"></div>
-            <div class="formatBtnContainer">
-                <button class="formatBtn" id="tagJL"><i class="fas fa-align-left"></i></button>
-                <div class="formatBtnDisc">Align text left</div>
-            </div>
-            <div class="formatBtnContainer">
-                <button class="formatBtn" id="tagJC"><i class="fas fa-align-center"></i></button>
-                <div class="formatBtnDisc">Align text center</div>
-            </div>
-            <div class="formatBtnContainer">
-                <button class="formatBtn" id="tagJR"><i class="fas fa-align-right"></i></button>
-                <div class="formatBtnDisc">Align text right</div>
-            </div>
-            <div class="formatBtnContainer">
-                <button class="formatBtn" id="tagJF"><i class="fas fa-align-justify"></i></button>
-                <div class="formatBtnDisc">Align text full</div>
-            </div>
-            <div class="formatBtnDiv"></div>
-            <div class="formatBtnContainer">
-                <button class="formatBtn" id=""><i class="fas fa-list-ul"></i></button>
-                <div class="formatBtnDisc">Unordered list</div>
-            </div>
-            <div class="formatBtnContainer">
-                <button class="formatBtn" id=""><i class="fas fa-list-ol"></i></button>
-                <div class="formatBtnDisc">Ordered list</div>
+            <div class="formatBtnGroup">
+                <div class="formatBtnContainer">
+                    <button class="formatBtn" id=""><i class="fas fa-list-ul"></i></button>
+                    <div class="formatBtnDisc">Unordered list</div>
+                </div>
+                <div class="formatBtnContainer">
+                    <button class="formatBtn" id=""><i class="fas fa-list-ol"></i></button>
+                    <div class="formatBtnDisc">Ordered list</div>
+                </div>
             </div>
         </div>
         <div id="noteContent" contenteditable="false"></div>
